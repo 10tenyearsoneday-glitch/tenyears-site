a6a716556009ffb464c1f1b3416e6aba10bb6c9a
10tenyearsoneday-glitch <10tenyears.oneday@gmail.com>
2026-01-02 00:00:45 +0800

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// 這裡放你的互動（之後可加：購物車、折扣公告、語言切換等）
-document.querySelectorAll('.nav-pill').forEach(a=>{
-  a.addEventListener('click', ()=>{
-    document.querySelectorAll('.nav-pill').forEach(x=>x.classList.remove('is-active'));
-    a.classList.add('is-active');
-  });
-});
